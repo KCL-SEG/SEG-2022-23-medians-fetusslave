463768a4893dcd13f4069f6f0b6ceccda77e964c
Hui Teo <hui.teo@kcl.ac.uk>
2023-09-25 15:13:49 +0100

    solution

--- median.py
+++ median.py
@@ -2,7 +2,7 @@
 """ENTER YOUR SOLUTION HERE!"""
 from math import ceil
 
-def median(*numbers):
+def median(numbers):
     try:
         numbers = sorted([float(value) for value in numbers])
         length = len(numbers)
@@ -10,4 +10,4 @@ def median(*numbers):
     except TypeError:
         print("some input is not number")
 
-print(median(1, 2, 3, 6, 10, 12))
+print(median([1, 2, 3, 6, 10, 12]))
