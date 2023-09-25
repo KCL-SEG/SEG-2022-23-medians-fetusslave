e790f7e871202176c2b1f1776924c1d1653c8e31
Hui Teo <hui.teo@kcl.ac.uk>
2023-09-25 15:11:43 +0100

    solution

--- median.py
+++ median.py
@@ -1,12 +1,13 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
+from math import ceil
 
-while True:
+def median(*numbers):
     try:
-        print("Enter a list of numbers separated by commas: ")
-        numbers = [float(value) for value in input().split(",")]
-    except ValueError:
-        print("Some input could not be converted to a number!")
-    else:
-        break
-print(numbers)
+        numbers = sorted([float(value) for value in numbers])
+        length = len(numbers)
+        return (numbers[ceil(length/2-1)]+numbers[length//2])/2
+    except TypeError:
+        print("some input is not number")
+
+print(median(1, 2, 3, 6, 10, 12))
